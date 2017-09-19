c1da73d693b707f083e46d2b1a16ac0e728474cd
KeisukeKudo <melchior.balthasar.casper326@gmail.com>
2017-09-19 14:10:20 +0900

    リファクタリング

--- FindPremiumFriday.py
+++ FindPremiumFriday.py
@@ -4,15 +4,17 @@ from flask import Flask
 
 api = Flask(__name__)
 
+YEAR_POSITION = 0
+MONTH_POSITION = 1
+
 
 @api.route('/premium-friday')
 def current_month_premium_friday():
     current_date = list(map(int, datetime.now().strftime('%Y,%m').split(',')))
-    year = current_date[0]
-    month = current_date[1]
+    year = current_date[YEAR_POSITION]
+    month = current_date[MONTH_POSITION]
 
-    last_date = datetime(year, month, monthrange(year, month)[1])
-    weekday = last_date.weekday()
+    last_date, weekday = last_date_weekday(year, month)
 
     return find_premium_friday(last_date, weekday)
 
@@ -20,26 +22,33 @@ def current_month_premium_friday():
 @api.route('/premium-friday/<int:year>/<int:month>', methods=['POST'])
 def parameter_find_premium_friday(year, month):
     try:
-        last_date = datetime(year, month, monthrange(year, month)[1])
-        weekday = last_date.weekday()
-        return find_premium_friday(last_date, weekday)
+        last_date, weekday = last_date_weekday(year, month)
 
+        return find_premium_friday(last_date, weekday)
     except ValueError:
         return 404
 
 
 def find_premium_friday(last_date, weekday):
-    for i in reversed(range(1, 8)):
-        if weekday == 3:
+
+    while weekday > -2:
+        if weekday == 4:
             break
 
-        if weekday == 0:
-            weekday += 6
+        if weekday == -1:
+            weekday += 7
 
         last_date -= timedelta(days=1)
         weekday -= 1
 
     return last_date.strftime('%Y-%m-%d')
+
+
+def last_date_weekday(year, month):
+    last_date = datetime(year, month, monthrange(year, month)[MONTH_POSITION])
+    weekday = last_date.weekday()
+
+    return last_date, weekday
 
 
 if __name__ == '__main__':
